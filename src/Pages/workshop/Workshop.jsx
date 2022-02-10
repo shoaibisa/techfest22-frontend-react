abb6ed1197aa8feb9fb9ac56fe4549a881bb549a
Gaurav <iamgaurav201@gmail.com>
2022-02-10 16:41:34 +0530

    pages add

---
+++ src/Pages/workshop/Workshop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Workshop.css';
+
+
+const Workshop = () => {
+    return(
+        <>
+        <h1>This is Workshop page</h1>
+        </>
+    );
+}
+export default Workshop;
