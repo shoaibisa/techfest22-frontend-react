b77cfa1c42a12ebdcc46a38fa1ced47995181620
Gaurav <iamgaurav201@gmail.com>
2022-02-10 16:36:07 +0530

    pages add

---
+++ src/Pages/domain/Domain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Domain.css';
+
+
+const Domain = () => {
+    return(
+        <>
+        <h1>This is domain page</h1>
+        </>
+    );
+}
+export default Domain;
