a5746569c8af46d8e5d8c90cd055daefbbaac972
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2022-03-21 21:35:30 +0530

    domain added

--- src/Pages/domain/Kermis/Kermis.jsx
+++ src/Pages/domain/Kermis/Kermis.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import './kermis.css';
+import React from "react";
+import './Kermis.jsx';
 import EventCard from "../../../components/subdomain/EventCard/EventCard";
 import EventLogo from "../../../components/subdomain/EventLogo/EventLogo";
 import pngwing from '../../../../src/images/Domain/pngwing 3.png';
+//import { useContext } from 'react';
+//import { ChemforContext } from './chemforContext';
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
+import { localUrl } from '../../../API/api';
 
-const Kermis = () => {
-  return (
-    <div className="subdomain">
-    <EventLogo imgsrc={pngwing}
-    title="Robozar" subTitle="They are doing what we do, eventually might do it all.
-    You build what can work for you, We bring this which can make you better at building. Do participate to build better."/>
-    <EventCard title ="Robozar"
-    subTitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui cumque quae accusantium ducimus iusto fugit officiis ad, libero animi aliquid! Qui minus nemo iure, dicta totam provident aliquam ducimus." amount="Rs. 50,000" date="11/03" time="11.59 P.M" mobile1="
-    +91XXXXXXXXXX" mobile2="+91XXXXXXXXXX"/>
-   
-    <EventCard title ="Robozar"
-    subTitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui cumque quae accusantium ducimus iusto fugit officiis ad, libero animi aliquid! Qui minus nemo iure, dicta totam provident aliquam ducimus." amount="Rs. 50,000" date="11/03" time="11.59 P.M" mobile1="
-    +91XXXXXXXXXX" mobile2="+91XXXXXXXXXX"/>
-    </div>
-  )
+
+const Chemfor = () => {
+     const [eData,setEdata]= useState();
+    useEffect(() => {
+        axios.get(`${localUrl}/event/getByDomain/kermis`).then(results => {
+             setEdata(results.data.data);
+           console.log(results.data.data);
+        });
+      }, []);
+    return(
+        <>
+       <div className="subdomain">
+        <EventLogo imgsrc={pngwing}
+        title="Kermis" subTitle="They are doing what we do, eventually might do it all.
+        You build what can work for you, We bring this which can make you better at building. Do participate to build better."/>
+        {
+            eData &&  eData.map((e)=>(
+            <EventCard key={e._id} title ={e.name} imgurl={`${localUrl}/profile/${e.photo}`}
+           subTitle={e.description} amount="Rs.300" endDate={e.endDate}  link={e.driveLink}  studentCoordinator={e.studentCoordinator} />
+            ))
+        }
+        
+       
+        </div>
+        </>
+
+    );
+  
 }
-
-export default Kermis;
+export default Chemfor;
--- src/Pages/domain/Plexus/Plexus.jsx
+++ src/Pages/domain/Plexus/Plexus.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
-import './Plexus.css';
+import React from "react";
 import EventCard from "../../../components/subdomain/EventCard/EventCard";
 import EventLogo from "../../../components/subdomain/EventLogo/EventLogo";
 import pngwing from '../../../../src/images/Domain/pngwing 3.png';
-const Plexus = () => {
-  return (
-    <div className="subdomain">
-    <EventLogo imgsrc={pngwing}
-    title="Robozar" subTitle="They are doing what we do, eventually might do it all.
-    You build what can work for you, We bring this which can make you better at building. Do participate to build better."/>
-    <EventCard title ="Robozar"
-    subTitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui cumque quae accusantium ducimus iusto fugit officiis ad, libero animi aliquid! Qui minus nemo iure, dicta totam provident aliquam ducimus." amount="Rs. 50,000" date="11/03" time="11.59 P.M" mobile1="
-    +91XXXXXXXXXX" mobile2="+91XXXXXXXXXX"/>
-   
-    <EventCard title ="Robozar"
-    subTitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui cumque quae accusantium ducimus iusto fugit officiis ad, libero animi aliquid! Qui minus nemo iure, dicta totam provident aliquam ducimus." amount="Rs. 50,000" date="11/03" time="11.59 P.M" mobile1="
-    +91XXXXXXXXXX" mobile2="+91XXXXXXXXXX"/>
-    </div>
-  )
-}
+//import { useContext } from 'react';
+//import { ChemforContext } from './chemforContext';
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
+import { localUrl } from '../../../API/api';
 
-export default Plexus;
+
+const Chemfor = () => {
+     const [eData,setEdata]= useState();
+    useEffect(() => {
+        axios.get(`${localUrl}/event/getByDomain/plexus`).then(results => {
+             setEdata(results.data.data);
+           console.log(results.data.data);
+        });
+      }, []);
+    return(
+        <>
+       <div className="subdomain">
+        <EventLogo imgsrc={pngwing}
+        title="Plexus" subTitle="They are doing what we do, eventually might do it all.
+        You build what can work for you, We bring this which can make you better at building. Do participate to build better."/>
+        {
+            eData &&  eData.map((e)=>(
+            <EventCard key={e._id} title ={e.name} imgurl={`${localUrl}/profile/${e.photo}`}
+           subTitle={e.description} amount="Rs.300" endDate={e.endDate}  link={e.driveLink}  studentCoordinator={e.studentCoordinator} />
+            ))
+        }
+        
+       
+        </div>
+        </>
+
+    );
+  
+}
+export default Chemfor;
