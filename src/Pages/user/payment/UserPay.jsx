a77c747296875a1ec54a021be631800b09618d82
shoaibisa <shoaibisa1@gmail.com>
2022-03-22 20:04:11 +0530

    payment done

--- src/Pages/user/payment/UserPay.jsx
+++ src/Pages/user/payment/UserPay.jsx
@@ -2,41 +2,62 @@
 import axios from 'axios';
 import React, { useContext, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { localUrl } from '../../../API/api';
+import { baseUrl, localUrl } from '../../../API/api';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../../auth/authContext';
 
 const UserPay = () => {
   const authContext = useContext(AuthContext);
-  useEffect(() => {});
 
-  const paySilver = () => {
-    return console.log(authContext.token);
-    axios.post(`${localUrl}/pay/user`).then(results => {
-      console.log(results);
-      // navigate(`${results.data.url}`)
-      // window.location.href = results.data.url
+  const paySilver = async () => {
+    const userPayData = {
+      price: 300,
+    };
+
+    const fetchdata = await axios({
+      method: 'post',
+      data: userPayData,
+      url: `${baseUrl}/pay/user`,
+      headers: {
+        Authorization: 'Bearer ' + authContext.token,
+      },
     });
+    console.log(fetchdata.data);
     console.log('clickef');
+    window.location.href = fetchdata.data.url;
   };
-  const payGold = () => {
-    axios.post(`${localUrl}/pay/user`).then(results => {
-      console.log(results);
-      // navigate(`${results.data.url}`)
-      // window.location.href = results.data.url
+  const payGold = async () => {
+    const userPayData = {
+      price: 600,
+    };
+
+    const fetchdata = await axios({
+      method: 'post',
+      data: userPayData,
+      url: `${baseUrl}/pay/user`,
+      headers: {
+        Authorization: 'Bearer ' + authContext.token,
+      },
     });
     console.log('clickef');
+    window.location.href = fetchdata.data.url;
   };
   return (
     <>
       <div className="main">
-        <div className="container-fluid mt-5 pt-5">
-          <Button variant="success" type="submit" block onClick={paySilver}>
-            <h1> 300 </h1>
+        <div className="container-fluid mt-5 pt-5 center">
+          <Button
+            variant="success"
+            style={{ marginBottom: '10px' }}
+            type="submit"
+            block
+            onClick={paySilver}
+          >
+            <h1> 300 Silver</h1>
           </Button>
-
+          <br />
           <Button variant="success" type="submit" block onClick={payGold}>
-            <h1> 400 </h1>
+            <h1> 600 Gold</h1>
           </Button>
         </div>
       </div>
