28fce62b6281dd8cfc8ef9749c0a5ac1a391bd5b
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2022-04-07 19:41:50 +0530

    done

--- src/Pages/AboutUs/aboutus.jsx
+++ src/Pages/AboutUs/aboutus.jsx
@@ -13,47 +13,13 @@ const Aboutus = () => {
           <h1 className="text-center  mb-5">Frequently Asked Questions</h1>
 
           <div className="row flex-center">
-            <div className="col-sm-10 offset-sm-2">
+           <div >   {/*  className="col-sm-10 offset-sm-2" */}
               <div className="accordion" id="accordionExample">
                 <div
-                  style={{ backgroundColor: 'transparent ' }}
-                  className="accordion-item"
-                >
-                  <h2 className="accordion-header" id="headingOne">
-                    <button
-                      style={{ backgroundColor: 'transparent' }}
-                      className="accordion-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseOne"
-                      aria-expanded="true"
-                      aria-controls="collapseOne"
-                    >
-                      <span style={{ color: 'white',background:'transparent' }}>
-                        How to Create FAQ Section
-                      </span>
-                    </button>
-                  </h2>
-                  <div
-                    id="collapseOne"
-                    className="accordion-collapse collapse show"
-                    aria-labelledby="headingOne"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div
-                      style={{ backgroundColor: 'transparent', color: 'white' }}
-                      className="accordion-body"
-                    >
-                      <strong>This is the first item's accordion body.</strong>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.{' '}
-                    </div>
-                  </div>
-                </div>
-                <div
-                  style={{ backgroundColor: 'transparent ' }}
+                  style={{
+                    backgroundColor: 'transparent ',
+                    borderBottom: '1px solid white',
+                  }}
                   className="accordion-item"
                 >
                   <h2 className="accordion-header" id="headingTwo">
@@ -66,20 +32,26 @@ const Aboutus = () => {
                       aria-expanded="false"
                       aria-controls="collapseTwo"
                     >
-                      <span style={{ color: 'white',background:'transparent'  }}>
+                      <span
+                        style={{ color: 'white', background: 'transparent' }}
+                      >
                         What jQuery do I need?
                       </span>
                     </button>
                   </h2>
                   <div
-                    style={{ color: 'white' }}
                     id="collapseTwo"
                     className="accordion-collapse collapse"
                     aria-labelledby="headingTwo"
                     data-bs-parent="#accordionExample"
+                    style={{ borderBottom: '1px sloid white', color: 'white' }}
                   >
                     <div
-                      style={{ backgroundColor: 'transparent', color: 'white' }}
+                      style={{
+                        backgroundColor: 'transparent',
+                        color: 'white',
+                        borderBottom: '1px sloid white',
+                      }}
                       className="accordion-body"
                     >
                       <strong>This is the second item's accordion body.</strong>
@@ -89,12 +61,60 @@ const Aboutus = () => {
                       quasi architecto beatae vitae dicta sunt explicabo. Nemo
                       enim ipsam voluptatem quia voluptas sit aspernatur aut
                       odit aut fugit, sed quia consequuntur magni dolores eos
-                      qui ratione voluptatem sequi nesciunt.{' '}
+                      qui ratione voluptatem sequi nesciunt.
+                    </div>
+                  </div>
+                </div>
+
+                <div
+                  style={{ backgroundColor: 'transparent' }}
+                  className="accordion-item"
+                >
+                  <h2 className="accordion-header" id="headingOne">
+                    <button
+                      style={{ backgroundColor: 'transparent' }}
+                      className="accordion-button"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      <span
+                        style={{ color: 'white', background: 'transparent' }}
+                      >
+                        How to Create FAQ Section
+                      </span>
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseOne"
+                    className="accordion-collapse collapse show"
+                    aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample"
+                    style={{ borderBottom: '1px sloid white' }}
+                  >
+                    <div
+                      style={{
+                        backgroundColor: 'transparent',
+                        color: 'white',
+                        borderBottom: '1px sloid white',
+                      }}
+                      className="accordion-body"
+                    >
+                      <strong>This is the first item's accordion body.</strong>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     </div>
                   </div>
                 </div>
                 <div
-                  style={{ backgroundColor: 'transparent ' }}
+                  style={{
+                    backgroundColor: 'transparent ',
+                    borderBottom: '1px solid white',
+                  }}
                   className="accordion-item"
                 >
                   <h2 className="accordion-header" id="headingThree">
@@ -107,7 +127,9 @@ const Aboutus = () => {
                       aria-expanded="false"
                       aria-controls="collapseThree"
                     >
-                      <span style={{ color: 'white',background:'transparent'  }}>
+                      <span
+                        style={{ color: 'white', background: 'transparent' }}
+                      >
                         HTML CSS FAQ Design With jQuery Accordion
                       </span>{' '}
                     </button>
@@ -117,9 +139,14 @@ const Aboutus = () => {
                     className="accordion-collapse collapse"
                     aria-labelledby="headingThree"
                     data-bs-parent="#accordionExample"
+                    style={{ borderBottom: '1px solid white' }}
                   >
                     <div
-                      style={{ backgroundColor: 'transparent', color: 'white' }}
+                      style={{
+                        backgroundColor: 'transparent',
+                        color: 'white',
+                        borderBottom: '1px sloid white',
+                      }}
                       className="accordion-body"
                     >
                       <strong>This is the third item's accordion body.</strong>
@@ -133,10 +160,21 @@ const Aboutus = () => {
                   </div>
                 </div>
               </div>
-              <div>
-                <h2 className="text-center  h2 mb-5">
-                  Don’t Think Twice , Simply Dial In
-                </h2>
+              <div
+               className='mt-5'
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <div>
+                
+                  <h2 className="text-center  h2 mb-5">
+                    Don’t Think Twice , Simply Dial In
+                  </h2>
+                </div>
+
                 <div
                   style={{ display: 'flex', justifyContent: 'space-evenly' }}
                   className="center"
@@ -146,44 +184,87 @@ const Aboutus = () => {
                       Soham
                     </p>
                     <p style={{ fontSize: '18px', marginRight: '25px' }}>
-                 
-                      <a style={{textDecoration:'none',color:'green'}}  href="tel:9740734156">9740734156</a>
+                      <a
+                        style={{ textDecoration: 'none', color: 'white' }}
+                        href="tel:9740734156"
+                      >
+                        9740734156
+                      </a>
                     </p>
                   </div>
                   <div>
                     <p style={{ fontSize: '20px', marginRight: '25px' }}>
                       Pragati
                     </p>
-                    <p style={{textDecoration:'none', fontSize: '18px', marginRight: '25px' }}>
+                    <p
+                      style={{
+                        textDecoration: 'none',
+                        fontSize: '18px',
+                        marginRight: '25px',
+                      }}
+                    >
                       {' '}
-                      <a style={{textDecoration:'none',color:'green'}}  href="tel:7367080390">7367080390</a>
+                      <a
+                        style={{ textDecoration: 'none', color: 'white' }}
+                        href="tel:7367080390"
+                      >
+                        7367080390
+                      </a>
                     </p>
                   </div>
                   <div>
-                    <p style={{ textDecoration:'none', fontSize: '20px', marginRight: '25px' }}>
+                    <p
+                      style={{
+                        textDecoration: 'none',
+                        fontSize: '20px',
+                        marginRight: '25px',
+                      }}
+                    >
                       Abhishek
                     </p>
-                    <p style={{ textDecoration:'none', fontSize: '18px', marginRight: '25px' }}>
-                      <a style={{textDecoration:'none' ,color:'green'}}  href="tel:7404146589">7404146589</a>
+                    <p
+                      style={{
+                        textDecoration: 'none',
+                        fontSize: '18px',
+                        marginRight: '25px',
+                      }}
+                    >
+                      <a
+                        style={{ textDecoration: 'none', color: 'white' }}
+                        href="tel:7404146589"
+                      >
+                        7404146589
+                      </a>
                     </p>
                   </div>
                 </div>
-              </div>
-              
-              
-            </div>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <div>
-                    <p style={{ fontSize: '20px', margin:'0px',  textAlign:'center' }}>
-                       write us at </p>
-                       <p style={{margin:'0px'}}>
-                        <a  style={{textDecoration:'none', color:'green'}}  href="#"><br></br>support@techfestsliet.com</a>
-                      </p>
-                  </div>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div>
+                  <p
+                    style={{
+                      fontSize: '20px',
+                      margin: '0px',
+                      textAlign: 'center',
+                    }}
+                  >
+                    write us at
+                  </p>
+                  <p style={{ margin: '-24px' }}>
+                    <a
+                      style={{ textDecoration: 'none', color: 'white' }}
+                      href="#"
+                    >
+                      <br></br>support@techfestsliet.com
+                    </a>
+                  </p>
                 </div>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <img className='imge' src={Freq} />
               </div>
+              </div>
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <img className="imge" src={Freq} />
+            </div>
           </div>
         </div>
       </section>
