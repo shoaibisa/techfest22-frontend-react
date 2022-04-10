c583023514b9644aed1614bf898cc1dcb14b9091
shoaibisa <shoaibisa1@gmail.com>
2022-04-10 11:32:44 +0530

    added user list

--- src/Pages/admin/userList/user.js
+++ src/Pages/admin/userList/user.js
@@ -8,9 +8,21 @@ const User = ({ user }) => {
       <td>
         {user.phone} {user.whatsappPhoneNumber}
       </td>
-      <td>{user.teamMembers.map(t => t.name + ', ')}</td>
-      <td>{user.events.map(e => e.name + ', ')}</td>
-      <td>{user.workshops.map(w => w.workshopName + ', ')}</td>
+      <td>
+        {!user.teamMembers || user.teamMembers.length === 0
+          ? 'None'
+          : user.teamMembers.map(t => t.name + ', ')}
+      </td>
+      <td>
+        {!user.events || user.events.length === 0
+          ? 'None'
+          : user.events.map(e => e.name + ', ')}
+      </td>
+      <td>
+        {!user.workshops || user.workshops.length === 0
+          ? 'None'
+          : user.workshops.map(w => w.workshopName + ', ')}
+      </td>
       {/* <td>
         <a href="#editEmployeeModal" className="edit" data-toggle="modal">
           <i
